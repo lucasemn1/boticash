b809fbdc061d389ce2b10f8f1816b1d1cd8349f1
Lucas Nóbrega <lucasnobrega.js@gmail.com.com>
2021-04-04 04:09:53 -0300

    Inserindo o título na página

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,6 +1,11 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 
 const Landing: FC = () => {
+  // onMounted
+  useEffect(() => {
+    document.title = "Boticash | Crie uma nova conta ou faça login.";
+  }, []);
+
   return (
     <h1>Landing Page</h1>
   );
