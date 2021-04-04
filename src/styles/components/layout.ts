fadae333e8407d3f53ad063bf60d277ce0d75254
Lucas Nóbrega <lucasnobrega.js@gmail.com.com>
2021-04-04 16:05:58 -0300

    Preparando a estilização das fontes

---
+++ src/styles/components/layout.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const FullPage = styled.div`
+  width: 100%;
+  max-width: 100vw;
+  height: 100vh;
+`;
+
+export const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,4 +12,24 @@ export default createGlobalStyle`
     background-color: ${props => props.theme.colors.getBackgroundColor()};
     color: ${props => props.theme.colors.getBackgroundContrastColor()};
   }
+
+  h1, h2, h3, h4, h5, h6, p {
+    font-weight: lighter;
+  }
+
+  h1 {
+    font-size: 50px;
+  }
+
+  h2 {
+    font-size: 35px;
+  }
+
+  h3 {
+    font-size: 22px;
+  }
+
+  p {
+    font-size: 15px;
+  }
 `;
